8cc8f2f14f3f0a68cabfb40cf072ea5bc35bc86c
Roy <roy-meissner@gmx.net>
2014-08-02 12:43:44 +0200

    Added contact form functionality

--- sendMail.php
+++ sendMail.php
@@ -1,20 +1,59 @@
 <?php
-    $email_to = "info@alternative-energienutzung.de";
-    $email_subject = "Anfrage von XYZ";
- 
-    $name = $_REQUEST['name'];
-    $email_from = $_REQUEST['email'];
-    $telephone = $_REQUEST['phone'];
-    $comments = $_REQUEST['comment'];
 
-    $email_message .= "Name: ".$name."\n";
-    $email_message .= "Email: ".$email_from."\n";
-    $email_message .= "Telephone: ".$telephone."\n";
-    $email_message .= "Comments: ".$comments."\n";
-		
-		$headers = 'From: '.$email_from."\r\n".
-		'Reply-To: '.$email_from."\r\n" .
-		'X-Mailer: PHP/' . phpversion();
-		 
-		@mail($email_to, $email_subject, $email_message, $headers);  
+	$to = "info@alternative-energienutzung.de";
+	
+	function died($error) {
+
+		echo "Es tut uns Leid, aber in ihren Eingaben ist ein Fehler aufgetreten.";
+		echo "<br /><br />";
+		echo $error."<br /><br />";
+		echo "Bitte gehen zurück und korrigieren Ihre Eingabe.<br /><br />";
+		die();
+  }
+
+	if(!isset($_POST['name']) ||
+    !isset($_POST['email']) ||
+    !isset($_POST['comment'])) {
+
+    died('Sie haben scheinbar eines der angegebenen Felder nicht ausgefüllt.');       
+
+  }
+  
+	$name = $_REQUEST['name'];
+	$from = $_REQUEST['email'];
+	$phone = $_REQUEST['phone'];
+	$comment = $_REQUEST['comment'];
+	
+	$error_message = "";
+  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$from)) {
+	  $error_message .= 'Die von Ihnen eigegebene E-Mail Adresse entspricht keinem bekannten Format.<br />';
+  }
+ 
+  if(strlen($comment) < 2) {
+    $error_message .= 'Der von ihnen eigegebene Text enthält keinen sinnvollen Inhalt.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+	  	
+	$subject = "Anfrage von " . $name;
+
+  $message .= "Name: ".$name."\n";
+  $message .= "Email: ".$from."\n";
+  $message .= "Telefon: ".$phone."\n";
+  $message .= "Nachricht: ".$comment."\n";
+	
+	$header = 'From: ' . $from . "\r\n" .
+  'Reply-To: ' . $from . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+	 
+	$mail = mail($to, $subject, $message, $header);  
+	if($mail){
+		echo "Wir haben Ihre Mail erhalten und werden Sie bald kontaktieren.";
+	}else{
+		echo "Beim versenden der Mail ist ein Fehler aufgetreten, der nicht an Ihnen liegt. Teilen Sie uns dies bitte auf einem anderem Weg mit, damit wir diesen beheben können."; 
+	}
 ?>
