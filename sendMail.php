a1c3c550dac0df00921b084ca251565816858189
Roy Meissner <roymeissn@googlemail.com>
2014-11-18 14:42:53 +0100

    rewrite of e-mail system

--- sendMail.php
+++ sendMail.php
@@ -1,43 +1,11 @@
 <?php
 
 	$to = "info@alternative-energienutzung.de";
-	
-	function died($error) {
-
-		echo "Es tut uns Leid, aber in ihren Eingaben ist ein Fehler aufgetreten.";
-		echo "<br /><br />";
-		echo $error."<br /><br />";
-		echo "Bitte gehen zurück und korrigieren Ihre Eingabe.<br /><br />";
-		die();
-  }
-
-	if(!isset($_POST['name']) ||
-    !isset($_POST['email']) ||
-    !isset($_POST['comment'])) {
-
-    died('Sie haben scheinbar eines der angegebenen Felder nicht ausgefüllt.');       
-
-  }
   
-	$name = $_REQUEST['name'];
-	$from = $_REQUEST['email'];
-	$phone = $_REQUEST['phone'];
-	$comment = $_REQUEST['comment'];
-	
-	$error_message = "";
-  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$from)) {
-	  $error_message .= 'Die von Ihnen eigegebene E-Mail Adresse entspricht keinem bekannten Format.<br />';
-  }
- 
-  if(strlen($comment) < 2) {
-    $error_message .= 'Der von ihnen eigegebene Text enthält keinen sinnvollen Inhalt.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
+	$name = $_POST['name'];
+	$from = $_POST['email'];
+	$phone = $_POST['phone'];
+	$comment = $_POST['message'];
 	  	
 	$subject = "Anfrage von " . $name;
 
@@ -49,11 +17,24 @@
 	$header = 'From: ' . $from . "\r\n" .
   'Reply-To: ' . $from . "\r\n" .
   'X-Mailer: PHP/' . phpversion();
-	 
+  
 	$mail = mail($to, $subject, $message, $header);  
+	
+	if (!function_exists('http_response_code')){
+		function http_response_code($newcode = NULL){
+			static $code = 200;
+			if($newcode !== NULL){
+				header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+				if(!headers_sent())
+					$code = $newcode;
+			}       
+			return $code;
+		}
+	}
+	
 	if($mail){
-		echo "Wir haben Ihre Mail erhalten und werden Sie bald kontaktieren.";
+		return true;
 	}else{
-		echo "Beim versenden der Mail ist ein Fehler aufgetreten, der nicht an Ihnen liegt. Teilen Sie uns dies bitte auf einem anderem Weg mit, damit wir diesen beheben können."; 
+		http_response_code(503);
 	}
 ?>
