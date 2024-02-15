3e32c47775c4a4713852ac1602485d83a68a95a1
Roy Meissner <meissner@informatik.uni-leipzig.de>
2024-02-15 12:50:48 +0100

    Applied image optimization and added a carousel item

--- sendMail.php
+++ sendMail.php
@@ -1,40 +1,45 @@
 <?php
 
-	$to = "info@alternative-energienutzung.de";
-  
-	$name = $_POST['name'];
-	$from = $_POST['email'];
-	$phone = $_POST['phone'];
-	$comment = $_POST['message'];
-	  	
-	$subject = "Anfrage von " . $name;
+  $to = "info@alternative-energienutzung.de";
+
+  $name = $_POST['name'];
+  $from = $_POST['email'];
+  $phone = $_POST['phone'];
+  $comment = $_POST['message'];
+
+  $subject = "Anfrage von " . $name;
+
+if (empty($name) || empty($from)) {
+    exit(0);
+}
 
   $message .= "Name: ".$name."\n";
   $message .= "Email: ".$from."\n";
   $message .= "Telefon: ".$phone."\n";
   $message .= "Nachricht: ".$comment."\n";
-	
-	$header = 'From: ' . $from . "\r\n" .
+
+  $header = 'From: ' . $from . "\r\n" .
   'Reply-To: ' . $from . "\r\n" .
   'X-Mailer: PHP/' . phpversion();
-  
-	$mail = mail($to, $subject, $message, $header);  
-	
-	if (!function_exists('http_response_code')){
-		function http_response_code($newcode = NULL){
-			static $code = 200;
-			if($newcode !== NULL){
-				header('X-PHP-Response-Code: '.$newcode, true, $newcode);
-				if(!headers_sent())
-					$code = $newcode;
-			}       
-			return $code;
-		}
-	}
-	
-	if($mail){
-		return true;
-	}else{
-		http_response_code(503);
-	}
+
+  $mail = mail($to, $subject, $message, $header);
+
+if (!function_exists('Http_Response_code')) {
+    function Http_Response_code($newcode = null)
+    {
+        static $code = 200;
+        if ($newcode !== null) {
+            header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+            if (!headers_sent())
+            $code = $newcode;
+        }
+        return $code;
+    }
+}
+
+if ($mail) {
+    return true;
+} else {
+    http_response_code(503);
+}
 ?>
